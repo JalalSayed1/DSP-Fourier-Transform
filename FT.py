f635c3b633a3ed027677a8e90aff7db59b32569e
2571964s <2571964s@student.gla.ac.uk>
2023-10-08 19:46:51 +0100

    Created sound clarify function to boost/damp certain frequencies + made a time domain plotting function and my_fft functions

--- FT.py
+++ FT.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import LogFormatter
 from scipy.io import wavfile
 
-# Constants
+# Constant.
 INT16_HALF_MAX = 2**15
 
 def read_and_normalize_wav(filename):
@@ -13,13 +13,18 @@ def read_and_normalize_wav(filename):
     time_axis = np.linspace(0, len(data)/rate, num=len(data))
     return rate, data_normalized, time_axis
 
-def plot_frequency_domain(rate, data, title):
-    '''Plots the frequency spectrum in dB scale.'''
-    plt.figure()
+def my_fft(data, rate):
+    '''Returns the frequency spectrum of the data.'''
     half_data_length = int(len(data)/2)
     fft_data = np.abs(np.fft.fft(data[:, 0])[0:half_data_length]) / len(data)
-    fft_data_db = 20 * np.log10(fft_data)
     freqs = np.linspace(0, rate/2, num=half_data_length)
+    return freqs, fft_data
+
+def plot_frequency_domain(fft_data, freqs, title):
+    '''Plots the frequency spectrum in dB scale.'''
+    plt.figure()
+    
+    fft_data_db = 20 * np.log10(fft_data)
     
     plt.semilogx(freqs, fft_data_db)
     plt.grid(True)
@@ -36,12 +41,58 @@ def plot_frequency_domain(rate, data, title):
     plt.title(title)
     # plt.show()
 
-# Read and normalize wav file
+def plot_time_domain(data, time_axis, title):
+    '''Plots the time domain.
+    data: raw data (not fft data)
+    '''
+    plt.figure()
+    plt.plot(time_axis, data)
+    plt.grid(True)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Amplitude")
+    plt.title(title)
+    # plt.show()
+
+def clarify_sound(fft_data, sampling_rate, low_freq, high_freq, damping_factor = 0.1, boost_factor = 10):
+    '''Clarifies the sound by damping all data then boosting the frequency spectrum between low_freq and high_freq.
+    damping_factor: the factor to damp all frequencies (linear scale). Default is 0.1.
+    boost_factor: the factor to boost the desired frequencies only (linear scale). Default is 10.
+    '''
+    
+    # index = freq/nyquist freq * number of samples. Nyquist freq = 1/2 * sampling rate 
+    low_index = int(low_freq / (sampling_rate/2) * len(fft_data))
+    high_index = int(high_freq / (sampling_rate/2) * len(fft_data))
+    
+    fft_data *= damping_factor # damp all frequencies
+    fft_data[low_index:high_index] *= boost_factor # boost the desired frequencies only
+    
+    return fft_data
+
+
+#' Read and normalize wav file
 rate1, data1, time1 = read_and_normalize_wav('sound1.wav')
 rate2, data2, time2 = read_and_normalize_wav('sound2.wav')
 
-# Plot in the frequency domain
-plot_frequency_domain(rate1, data1, 'sound1.wav')
-plot_frequency_domain(rate2, data2, 'sound2.wav')
+#' Plot in the frequency domain
+freqs1, fft_data1 = my_fft(data1, rate1)
+freq2, fft_data2 = my_fft(data2, rate2)
+
+plot_frequency_domain(fft_data1, freqs1, "Frequency domain of sound1.wav")
+plot_frequency_domain(fft_data2, freq2, "Frequency domain of sound2.wav")
+
+#' Clarify the sound
+fft_data1 = clarify_sound(fft_data1, rate1, 1000, 2000)
+fft_data2 = clarify_sound(fft_data2, rate2, 1000, 2000)
+
+plot_frequency_domain(fft_data1, freqs1, "Frequency domain of sound1.wav after clarification")
+plot_frequency_domain(fft_data2, freq2, "Frequency domain of sound2.wav after clarification")
+
+
+
+
+
+
+
+
 
 plt.show()
