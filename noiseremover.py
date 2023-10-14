76a2941a7cdcccb89b43fc86d8e13cd70d61c576
2571964s <2571964s@student.gla.ac.uk>
2023-10-14 19:36:44 +0100

    Changed the code to be able to plot multiple frequency responces on same graph

--- noiseremover.py
+++ noiseremover.py
@@ -22,6 +22,16 @@ def main():
     sampling_rate2, data2, freqs2, fft_data2, time2 = read_file(
         "original1m.wav")
 
+    figure_frequency_domain_1 = plt.figure()
+    figure_frequency_domain_2 = plt.figure()
+
+    plt.figure(figure_frequency_domain_1.number)
+    figure_frequency_domain_1 = plot_frequency_domain(np.abs(
+        fft_data1), freqs1, legend="original1cm.wav", figure=figure_frequency_domain_1)
+    plt.figure(figure_frequency_domain_2.number)
+    figure_frequency_domain_2 = plot_frequency_domain(np.abs(
+        fft_data2), freqs2, legend="original1m.wav", figure=figure_frequency_domain_2)
+
     max_freq1 = index_to_freq(len(fft_data1)-1, sampling_rate1, len(fft_data1))
     max_freq2 = index_to_freq(len(fft_data2)-1, sampling_rate2, len(fft_data2))
 
@@ -30,6 +40,13 @@ def main():
         fft_data1, sampling_rate1, 70, 3000, damping_factor=0.5, boost_factor=30)
     fft_data_clarified2 = clarify_sound(
         fft_data2, sampling_rate2, 70, 3000, damping_factor=0.9, boost_factor=50)
+
+    plt.figure(figure_frequency_domain_1.number)
+    figure_frequency_domain_1 = plot_frequency_domain(np.abs(fft_data_clarified1), freqs1, legend="improved1cm.wav",
+                                                      figure=figure_frequency_domain_1)
+    plt.figure(figure_frequency_domain_2.number)
+    figure_frequency_domain_2 = plot_frequency_domain(np.abs(fft_data_clarified2), freqs2, legend="improved1m.wav",
+                                                      figure=figure_frequency_domain_2)
 
     fft_data_noise_removed1 = remove_noise(
         fft_data_clarified1, sampling_rate1, 0, 70, 0.001)
@@ -45,33 +62,34 @@ def main():
     write_wav("noiseremoved1cm.wav", sampling_rate1, fft_data_clarified1)
     write_wav("noiseremoved1m.wav", sampling_rate2, fft_data_clarified2)
 
-    # plot freq domain before and after:
-    figure_frequency_domain_1 = plt.figure()
-    figure_frequency_domain_1 = plot_frequency_domain(np.abs(
-        fft_data1), freqs1, legend="original1cm.wav", figure=figure_frequency_domain_1)
+    # ' plot freq domain before and after
+    plt.figure(figure_frequency_domain_1.number)
     figure_frequency_domain_1 = plot_frequency_domain(np.abs(
         fft_data_noise_removed1), freqs1, legend="noiseremoved1cm.wav", figure=figure_frequency_domain_1)
-
-    figure_frequency_domain_2 = plt.figure()
-    figure_frequency_domain_2 = plot_frequency_domain(np.abs(
-        fft_data2), freqs2, legend="original1m.wav", figure=figure_frequency_domain_2)
+    plt.figure(figure_frequency_domain_2.number)
     figure_frequency_domain_2 = plot_frequency_domain(np.abs(
         fft_data_noise_removed2), freqs2, legend="noiseremoved1m.wav", figure=figure_frequency_domain_2)
 
-    # plot time domain before and after:
+    # ' plot time domain before and after
     figure_time_domain_1 = plt.figure()
     data_noise_removed1 = my_ifft(fft_data_noise_removed1)
+
+    figure_time_domain_1 = plot_time_domain(
+        data_noise_removed1, time1, legend="noiseremoved1cm.wav", figure=figure_time_domain_1)
     figure_time_domain_1 = plot_time_domain(
         data1, time1, legend="original1cm.wav", figure=figure_time_domain_1)
     figure_time_domain_1 = plot_time_domain(
-        data_noise_removed1, time1, legend="noiseremoved1cm.wav", figure=figure_time_domain_1)
+        data1, time1, legend="improved1cm.wav", figure=figure_time_domain_1)
 
     figure_time_domain_2 = plt.figure()
     data_noise_removed2 = my_ifft(fft_data_noise_removed2)
+
+    figure_time_domain_2 = plot_time_domain(
+        data_noise_removed2, time2, legend="noiseremoved1m.wav", figure=figure_time_domain_2)
     figure_time_domain_2 = plot_time_domain(
         data2, time2, legend="original1m.wav", figure=figure_time_domain_2)
     figure_time_domain_2 = plot_time_domain(
-        data_noise_removed2, time2, legend="noiseremoved1m.wav", figure=figure_time_domain_2)
+        data2, time2, legend="improved1m.wav", figure=figure_time_domain_2)
 
     figure_time_domain_1.legend()
     figure_time_domain_2.legend()
